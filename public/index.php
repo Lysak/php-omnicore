af2a913cc1e6669e4f4aa971209b15e7c1b9027d
Dmytrii Lysak <dlysak@live.com>
2021-01-17 19:51:45 +0200

    Revert "rc #3"
    This reverts commit 5be37147

--- public/index.php
+++ public/index.php
@@ -16,12 +16,12 @@ $userDto = $dtoFactory->create(UserDto::class);
 $user = new User($userDto);
 
 header('Content-Type: application/json');
-echo json_encode($user->getErrors());
+http_response_code(200);
 
 if (!$user->validate()) {
     http_response_code(400);
+    echo json_encode($user->getErrors());
     return false;
-} else {
-    http_response_code(200);
-    return true;
 }
+
+return true;
